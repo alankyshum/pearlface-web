8c6c3ca572985f7d6d776e0d01498560f87416b7
Alan Shum <alan200994@gmail.com>
2019-09-15 17:58:27 -0700

    MISC | router + types done

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,7 +2,8 @@ import Route from '@ember/routing/route';
 import webpageContent from 'pearlface-web/constants/webpage-content';
 
 export default class IndexRoute extends Route {
+  /** @returns {Webpage.Content} */
   model() {
-    return webpageContent;
+    return /** @type {Webpage.Content} */ (webpageContent);
   }
 }
